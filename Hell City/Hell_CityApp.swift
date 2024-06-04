646a8836d535b81bfdfa856c639bc4632e222b03
Gabriel-M-Martins <gabrielmmartinscontato@gmail.com>
2024-06-04 12:22:24 -0300

    Initial Commit

---
+++ Hell City/Hell_CityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hell_CityApp.swift
+//  Hell City
+//
+//  Created by Gabriel Medeiros Martins on 04/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Hell_CityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
